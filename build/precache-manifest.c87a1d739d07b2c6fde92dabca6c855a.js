3e624b5b7814b9d2a7116debf5f10ae3cc5762bf
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-13 20:28:19 -0300

    build

---
+++ build/precache-manifest.c87a1d739d07b2c6fde92dabca6c855a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd5a695bd49a0ac2d15431094718dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "659c37d80e93d117472e",
+    "url": "/static/css/main.0cf28149.chunk.css"
+  },
+  {
+    "revision": "060fec65b8be89170fa8",
+    "url": "/static/js/2.5884c943.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5884c943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659c37d80e93d117472e",
+    "url": "/static/js/main.60d2e586.chunk.js"
+  },
+  {
+    "revision": "3d22c4ff75fc8bca25f7",
+    "url": "/static/js/runtime-main.68c0a999.js"
+  },
+  {
+    "revision": "652647e7c14f73e079f1418dc4bd2ae8",
+    "url": "/static/media/starwars_planets.652647e7.png"
+  }
+]);
